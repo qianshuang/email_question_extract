24a8b1a74099e9b4f04f73ecdd8fcf0e0821d8f4
EDY <qs@anfan.com>
2021-10-11 22:41:39 +0800

    update

--- main.py
+++ main.py
@@ -20,16 +20,22 @@ for k, v in df.items():
     df_final = pd.concat([df_final, v_df], ignore_index=True)
 
 df_final = df_final.dropna(subset=["Incoming email content"])  # 81行
+df_final['Incoming email content'] = df_final['Incoming email content'].apply(
+    lambda x: str(x).replace('\t', ' ').replace('\n', ' '))
 print(df_final.head())
 print(df_final.shape)
 
 all_qs = []
+stop_sents = read_file("resources/stop_sents.txt")
 
 for index, row in df_final.iterrows():
     Incoming_email_content = row["Incoming email content"]
     if (not pd.isnull(row["Incoming email subject"])) and len(Incoming_email_content) < len(
             row["Incoming email subject"]):
         Incoming_email_content = row["Incoming email subject"]
+
+    # 指代消歧
+    Incoming_email_content = nlp(Incoming_email_content)._.coref_resolved
 
     acc_questions = []
     sent_tokenize_list = sent_tokenize(Incoming_email_content)
@@ -52,6 +58,7 @@ for index, row in df_final.iterrows():
             token = doc[i_]
             if token.pos_ == "VERB" and token.dep_ == "ROOT":
                 verbs.append(token.lemma_)
+
             if token.pos_ == "NOUN" and token.dep_ == "dobj":
                 dobjs.append(token.lemma_)
 
@@ -59,16 +66,14 @@ for index, row in df_final.iterrows():
             if ("resolve" in verbs and ("issue" in dobjs or "problem" in dobjs)) or \
                     ("answer" in verbs and "question" in dobjs):
                 continue
-        # 3. 指代消歧
-        acc_questions.append(str(i) + ". " + doc._.coref_resolved)
 
-    all_qs.append("\n".join(acc_questions))
+        for st in stop_sents:
+            
+
+        acc_questions.append(str(i) + ". " + s)
+
+    all_qs.append(" ".join(acc_questions))
 df_final["extract_questions"] = all_qs
 print(df_final)
-
-df_final['Incoming email content'] = df_final['Incoming email content'].apply(
-    lambda x: str(x).replace('\t', ' ').replace('\n', ' '))
-df_final['extract_questions'] = df_final['extract_questions'].apply(
-    lambda x: str(x).replace('\t', ' ').replace('\n', ' '))
 
 df_final.to_csv('data/email_question_result.txt', sep='\t', index=False)
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
-print("".join([]))
+import spacy
+import neuralcoref
+
+nlp = spacy.load('en_core_web_sm')  # 加载预训练模型
+neuralcoref.add_to_pipe(nlp)
+
+s = """Dear Sir or Madam,  I used to register +1 805 2354 as my contact number. But now I have a new number +1 525 6666 and I want to use this as my contact number regarding my accounts. Could you please update it for me? Thanks, Jes"""
+doc = nlp(s)
+print(doc._.coref_resolved)
